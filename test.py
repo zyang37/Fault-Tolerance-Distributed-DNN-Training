823fded660fd9a682383304d951c60586e0549e5
Aditya Guin <adityaguin@gmail.com>
2023-10-19 17:43:11 -0400

    Added DDP class and function to distribute healthy datasets to faulty workers

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ torch.manual_seed(seed_value)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
+
 # Define model
 class SimpleModel(nn.Module):
     def __init__(self):
@@ -23,15 +24,18 @@ class SimpleModel(nn.Module):
     def forward(self, x):
         return self.fc(x)
 
+
 def generate_data(num_samples):
     x = np.linspace(-1, 1, num_samples)
     y = x ** 3
     return torch.tensor(x, dtype=torch.float32).view(-1, 1), torch.tensor(y, dtype=torch.float32).view(-1, 1)
 
+
 # Divide a tensor into a specific number of sub-batches
 def divide_into_sub_batches(tensor, num_sub_batches):
     sub_batch_size = tensor.size(0) // num_sub_batches
     return [tensor[i:i + sub_batch_size] for i in range(0, tensor.size(0), sub_batch_size)]
+
 
 # Worker process
 def worker(model, data, target, gradients_list, loss_list, optimizer):
@@ -43,12 +47,12 @@ def worker(model, data, target, gradients_list, loss_list, optimizer):
     gradients_list.append(gradients)
     loss_list.append(loss.item())
 
+
 if __name__ == '__main__':
     manager = Manager()
 
     num_samples = 200
     x_data, y_data = generate_data(num_samples)
-
     # Create DataLoader
     dataset = TensorDataset(x_data, y_data)
     data_loader = DataLoader(dataset, batch_size=40, shuffle=True)
@@ -72,7 +76,8 @@ if __name__ == '__main__':
 
             processes = []
             for data_sub_batch, target_sub_batch in zip(data_sub_batches, target_sub_batches):
-                p = Process(target=worker, args=(global_model, data_sub_batch, target_sub_batch, gradients_list, loss_list, optimizer))
+                p = Process(target=worker,
+                            args=(global_model, data_sub_batch, target_sub_batch, gradients_list, loss_list, optimizer))
                 p.start()
                 processes.append(p)
 
@@ -90,8 +95,8 @@ if __name__ == '__main__':
 
             avg_iter_loss = sum(loss_list) / len(loss_list)
             epoch_loss_list.append(avg_iter_loss)
-            print(f'Epoch: {epoch+1}, sub-batch: {iteration}, avg sub-batch loss: {round(avg_iter_loss, 3)}')
+            print(f'Epoch: {epoch + 1}, sub-batch: {iteration}, avg sub-batch loss: {round(avg_iter_loss, 3)}')
 
         # Compute and print the average epoch loss
         avg_epoch_loss = sum(epoch_loss_list) / len(epoch_loss_list)
-        print(f'Epoch: {epoch+1} done, avg loss: {round(avg_epoch_loss, 3)}')
+        print(f'Epoch: {epoch + 1} done, avg loss: {round(avg_epoch_loss, 3)}')
